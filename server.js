b83cf33adc2a28756b6166527808ec9162b116f9
zoey-yjj <zoeyyyjj@gmail.com>
2023-09-23 10:36:19 +0800

    add cors, cors options, express json, cookie parser

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const PORT = process.env.PORT || 3500
+
+app.use(cors(corsOptions))
+
+app.use(express.json())
+
+app.use(cookieParser())
 
 app.use('/', express.static(path.join(__dirname, '/public')))
 
